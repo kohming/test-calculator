e5381b389751d6c1e7cf26c3724695c40dc361ef
Popy Febrian <pfebrian@gmail.com>
2019-04-11 16:16:48 +0700

    perbaikan 01

--- src/main/java/test/Calculator.java
+++ src/main/java/test/Calculator.java
@@ -2,7 +2,7 @@ package test;
 
 public class Calculator {
 
-	public static void (String[] args) {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Calculator c = new Calculator();
 		System.out.println(c.add(1, 1));
