56803c8f5a4a19891ebe1cf2c4e38cd24bdc39df
Popy Febrian <pfebrian@gmail.com>
2019-04-11 15:09:34 +0700

    adding files

---
+++ src/main/java/test/Calculator.java
@@ -0,0 +1,25 @@
+package test;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator c = new Calculator();
+		System.out.println(c.add(1, 1));
+	}
+
+	public Integer add(int i, int j) {
+		// TODO Auto-generated method stub
+		return i + j;
+	}
+	
+	public Integer multiply(int i, int j) {
+		// TODO Auto-generated method stub
+		return i * j;
+	}
+	
+	public Integer devide(int i, int j) {
+		// TODO Auto-generated method stub
+		return i / j;
+	}
+}
