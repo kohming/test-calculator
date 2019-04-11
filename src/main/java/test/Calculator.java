cd5d3f9654cdd34e11f0e64cccd122ac3660a001
Popy Febrian <pfebrian@gmail.com>
2019-04-11 15:51:09 +0700

    test error

--- src/main/java/test/Calculator.java
+++ src/main/java/test/Calculator.java
@@ -4,7 +4,7 @@ public class Calculator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Calculator c = new Calculator();
+		Calculator c = new Calculator(;
 		System.out.println(c.add(1, 1));
 	}
 
